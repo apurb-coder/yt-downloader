bd8867feb3782b0bb9a7c54f249f03b61719ce34
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-04-02 10:48:24 +0530

    Refactor DownloadPage component

--- frontend/src/components/DownloadPage.jsx
+++ frontend/src/components/DownloadPage.jsx
@@ -8,18 +8,17 @@ const DownloadPage = () => {
   const {qualityOptions } = useApi();
   const [optionsQualityObject, setOptionsQualityObject] = useState({});
   useEffect(()=>{
-    qualityOptions.map((quality) => {
-      const newOptionsQualityObject = qualityOptions.map((quality) => ({
-        value: quality,
-        label: quality,
-      }));
-      setOptionsQualityObject(newOptionsQualityObject)
-    });
+    const newOptionsQualityObject = qualityOptions.map((quality) => ({
+      value: quality,
+      label: quality,
+    }));
+    setOptionsQualityObject(newOptionsQualityObject);
   },[qualityOptions])
   const customStyles = {
     option: (provided, state) => ({ // styles for option element itself
       ...provided,
       borderRadius: "0.5rem",
+      width:"100%",
       borderColor: "rgba(107, 114, 128, 0.5)",
       outline: "none",
     }),
@@ -46,22 +45,25 @@ const DownloadPage = () => {
             styles={customStyles}
             defaultValue={optionsQualityObject[0]}
             options={optionsQualityObject}
+            className="mt-7"
           />
         </div>
-        <button className="relative inline-block text-sm lg:text-lg group w-[8.5rem] lg:w-[10rem] mt-7 lg:mt-16">
-          <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-red-600 transition-colors duration-300 ease-out border-2 border-red-700 rounded-lg group-hover:text-white">
-            <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-            <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-red-700 group-hover:-rotate-180 ease"></span>
-            <span className="relative flex items-center justify-center">
-              Download{" "}
-              <IoIosCloudDownload className="ml-3 text-red-600 group-hover:text-white" />
+        <div className="z-0">
+          <button className="relative inline-block text-sm lg:text-lg group w-[8.5rem] lg:w-[10rem] mt-7 lg:mt-16">
+            <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-red-600 transition-colors duration-300 ease-out border-2 border-red-700 rounded-lg group-hover:text-white">
+              <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+              <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-red-700 group-hover:-rotate-180 ease"></span>
+              <span className="relative flex items-center justify-center">
+                Download{" "}
+                <IoIosCloudDownload className="ml-3 text-red-600 group-hover:text-white" />
+              </span>
             </span>
-          </span>
-          <span
-            className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-red-600 rounded-lg group-hover:mb-0 group-hover:mr-0"
-            data-rounded="rounded-lg"
-          ></span>
-        </button>
+            <span
+              className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-red-600 rounded-lg group-hover:mb-0 group-hover:mr-0"
+              data-rounded="rounded-lg"
+            ></span>
+          </button>
+        </div>
       </div>
     </div>
   );

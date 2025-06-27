f2578044a9838073c5c7d267430fee4ef8833933
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2025-06-27 12:56:05 +0530

    .

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <nav className="flex p-4 md:p-6 bg-white">
         <div className="pl-2 md:pl-6 flex justify-center items-center space-x-2 text-xl md:text-2xl">
           <SiYoutube className="text-red-600" />
-          <span className="text-gray-800 font-bold">YouTube Downloader</span>
+          <span className="text-gray-800 font-bold">YT Fetch</span>
         </div>
       </nav>
     </>
